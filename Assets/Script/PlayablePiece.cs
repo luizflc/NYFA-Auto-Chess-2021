f8f1713493d04e088df96cdc766b15ec2ff28d9f
Mi Diaz <47762765+SkullHeadGMZ@users.noreply.github.com>
2021-01-20 16:50:14 -0800

    Shapes and colors in

---
+++ Assets/Script/PlayablePiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayablePiece : MonoBehaviour
+{
+    public ColorStats color;
+    public ShapeStats shape;
+    // Start is called before the first frame update
+    public int damage;
+    public int health;
+    public int cost;
+    public int dmgRadius;
+    public int speed;
+    void Start()
+    {
+        damage = shape.damage;
+        health = shape.health;
+        cost = shape.cost;
+        dmgRadius = color.dmgRadius;
+        speed = color.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
