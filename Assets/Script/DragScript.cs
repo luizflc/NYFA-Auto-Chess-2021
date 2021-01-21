3df7aff0f829cb8b3796cb415ba42df551535bbc
LukeSapir <58094923+LukeSapir@users.noreply.github.com>
2021-01-20 18:22:26 -0800

    Added Drag & Drop Script

---
+++ Assets/Script/DragScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+
+    public bool inBattle = false;
+    public bool dragging = false;
+    float distance;
+
+    void Start()
+    {
+
+    }
+    private void OnMouseDown()
+    {
+        if (!inBattle)
+        {
+            distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+            dragging = true;
+        }
+    }
+    private void OnMouseUp()
+    {
+        dragging = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = myRay.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
