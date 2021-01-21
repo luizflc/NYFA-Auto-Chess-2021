3726661532b3021817cfdd4166bfbe8fe4058788
templar45 <57818640+templar45@users.noreply.github.com>
2021-01-20 17:38:35 -0800

    Chris - example/testing scripts
    one script that contains enum and the other that register to player input

---
+++ Assets/Scenes/InputTesting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTesting : MonoBehaviour
+{
+    private BattlePhases gamePhases;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePhases = GetComponent<BattlePhases>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            gamePhases.playerState = BattlePhases.GameState.PlayerAttack;
+        }
+    }
+}
