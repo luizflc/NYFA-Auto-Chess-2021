56d5246e7fd19cee9d5e64c8cd92d6167da544f0
LukeSapir <58094923+LukeSapir@users.noreply.github.com>
2021-01-27 13:49:47 -0800

    Added Simple Death Script - Luke
    Put in a Death Script, needs a playable piece script on the object to work.

---
+++ Assets/Script/PieceDeathScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceDeathScript : MonoBehaviour
+{
+    //Author: Luke Sapir
+
+    public PlayablePiece myPlayable;
+    bool dead;
+    public Vector3 afterlife;
+    public GameObject Graveyard;
+    void Start()
+    {
+         myPlayable = gameObject.GetComponent<PlayablePiece>();
+         dead = false;
+        if (Graveyard == null)
+        {
+            afterlife = new Vector3(10, 10, 10);
+        }
+        else
+        {
+            afterlife = Graveyard.transform.position;
+        }
+    }
+
+   
+    void Update()
+    {
+     if(myPlayable.health <= 0 && dead == false)
+        {
+            Death();
+        }   
+    }
+    void Death()
+    {
+        dead = true;
+        transform.position = afterlife;
+        gameObject.SetActive(false);
+    }
+}
