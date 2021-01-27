774cbeddaf58c32c8a025d6f0c1e7e1a8bbc1e76
Mi Diaz <47762765+SkullHeadGMZ@users.noreply.github.com>
2021-01-27 15:13:18 -0800

    Quick fix

---
+++ Assets/Script/BounceOff.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        print(col.name);
+        if(col.gameObject.GetComponent<PlayablePiece>() == true) {
+            PlayablePiece currPiece = col.gameObject.GetComponent<PlayablePiece>();
+            currPiece.speed *= -1;
+            /*currPiece.goingForward = !currPiece.goingForward;
+            currPiece.sideStrafe = !currPiece.sideStrafe;*/
+            print(col.name + "has bounced");
+        }
+    }
+}
