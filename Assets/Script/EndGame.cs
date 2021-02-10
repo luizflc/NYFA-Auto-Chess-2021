16baa6a59db489214e786df419dfb60f6d263c62
JordanBarboza <36711451+JordanBarboza@users.noreply.github.com>
2021-02-10 15:08:52 -0800

    Started work on Menu and added app quit script

---
+++ Assets/Script/EndGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Author Jordan Barboza
+
+
+
+public class EndGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    // Ends game when escape is pressed
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+	}
+
+}
+
---
+++ Assets/Script/StartMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("BattleBoard");
+    }
+
+    /*public void CreditsButton()
+    {
+        SceneManager.LoadScene("Credits");
+    } */
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
