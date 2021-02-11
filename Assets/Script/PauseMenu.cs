2fc5caf7f2f36d2f8a3d0ffe0e294b855ef97131
templar45 <57818640+templar45@users.noreply.github.com>
2021-02-10 16:37:01 -0800

    Chris's push
    Chris Cruz - pause menu and movingtest

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string thisLevel;
+    public GameObject pauseMenu;
+    public bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //thisLevel = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player input to display pause menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            pauseMenu.SetActive(isPaused);
+            if (isPaused)
+            {
+                //freezes game if paused
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void RetryGame()
+    {
+        //reloads scene
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        //SceneManager.LoadScene(thisLevel);
+    }
+
+    public void Quit()
+    {
+        //load game back to menu
+        Time.timeScale = 1;
+        //SceneManager.LoadScene("StartMenu");
+    }
+}
