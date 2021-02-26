0c9c5e2d5e9a3839ff36ff8d4413de7e99ccfdc3
JordanBarboza <36711451+JordanBarboza@users.noreply.github.com>
2021-02-26 10:24:25 -0800

    Health Bar
    Created Health bar and attached them to the game piece prefabs. You can press "K" to decrease health and "H" in increase it. This will be a placeholder until we can make it lower via hitting a piece.

---
+++ Assets/Script/HealthTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTest : MonoBehaviour
+{
+    //Author Jordan Barboza
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthBar.SetHealthBarValue(1);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey("k"))
+        {
+            HealthBar.SetHealthBarValue(HealthBar.GetHealthBarValue() - 0.03f);
+        }
+
+        if (Input.GetKey("h"))
+        {
+            HealthBar.SetHealthBarValue(HealthBar.GetHealthBarValue() + 0.03f);
+        }
+    }
+}
