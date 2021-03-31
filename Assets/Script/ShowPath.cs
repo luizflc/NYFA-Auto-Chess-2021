50684530c950f5bb52128c497d8e689d136eb3db
Mi Diaz <47762765+SkullHeadGMZ@users.noreply.github.com>
2021-03-31 13:46:36 -0700

    pushing path

---
+++ Assets/Script/ShowPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPath : MonoBehaviour
+{
+    public GameObject path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        print("Mousing Over");
+        path.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        path.SetActive(false);
+        print("leaving object");
+    }
+}
