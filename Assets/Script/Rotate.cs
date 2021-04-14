2f16b1c208222915b5e5e1ac76b59d25b6f71f92
JordanBarboza <36711451+JordanBarboza@users.noreply.github.com>
2021-04-14 16:37:27 -0700

    Changes to UI and objects in BattleBoard Scene

---
+++ Assets/Script/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    float m_timer = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer -= Time.deltaTime;
+        if (m_timer <= 0.0f)
+        {
+            m_timer += 3.0f;
+            transform.Rotate( 0, 180f, 0);
+        }
+    }
+}
