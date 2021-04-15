a2dd8970bba521097fce11505cd7960459ba7c2b
Mi Diaz <47762765+SkullHeadGMZ@users.noreply.github.com>
2021-04-14 17:06:59 -0700

    Pushing AI

--- Assets/Script/GameManagerV2.cs
+++ Assets/Script/GameManagerV2.cs
@@ -9,22 +9,23 @@ public class GameManagerV2 : MonoBehaviour
     public Vector3[] startPoints;
     public int roundNum;
     
-   /// public float timerStart;
-   // float timer;
+     public float timerStart = 3;
+     float timer;
    // public int score;
     //public Text timerText;
     // Start is called before the first frame update
     void Start()
     {
         roundNum = 1;
-  //      timer = timerStart;
+       timer = timerStart;
         StartRound();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
+
+        
         if(timer > 0f)
         {
             timer -= Time.deltaTime;
@@ -32,10 +33,11 @@ public class GameManagerV2 : MonoBehaviour
         }
        else if(timer <= 0f)
         {
-            timer = timerStart;
-            EndRound();
+            SpawnPiece();
+            timer = Random.Range(0f,5f);
+            //EndRound();
         } 
-        */
+        
     }
 
     public void StartRound() {
@@ -44,15 +46,15 @@ public class GameManagerV2 : MonoBehaviour
 
     public void SpawnPiece()
     {
-        for(int i = 0; i < roundNum; i++)
-        {
+        //for(int i = 0; i < roundNum; i++)
+        //{
             int pieceNum = Random.Range(0, Pieces.Length);
             print("Piece num is " + pieceNum);
             int startPos = Random.Range(0, startPoints.Length);
             print("StartPos num is " + startPos);
             GameObject newEnemyPiece = Instantiate(Pieces[pieceNum], startPoints[startPos], Quaternion.Euler(-90,-90,0));
             newEnemyPiece.tag = "EnemyPiece";
-        }
+        //}
     }
 
     public void EndRound()
