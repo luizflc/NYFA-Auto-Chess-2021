846b1cc27ac9e62dbadee80d35001e2d64829efa
JordanBarboza <36711451+JordanBarboza@users.noreply.github.com>
2021-04-21 13:52:35 -0700

    Level Select scene and changes to build settings

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    float m_timer = 3.0f;
+    public float m_timer;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,37 @@ public class Rotate : MonoBehaviour
         if (m_timer <= 0.0f)
         {
             m_timer += 3.0f;
-            transform.Rotate( 0, 180f, 0);
+            transform.Rotate( 0, 0, 180f);
+            print("Rotating"); 
         }
+    } 
+
+   /* public void OnTriggerEnter(Collider col)
+    {
+        if ((col.gameObject.tag == "Rotate"))
+        {
+            transform.Rotate(0f, 0f, +180.0f);
+            Destroy(col.gameObject);
+            print("Rotating");
+        }
+
+        else if ((col.gameObject.tag == "NoRotate"))
+        {
+            transform.Rotate(0f, 0f, -180.0f);
+            Destroy(col.gameObject);
+            print("Rotating");
+        }
+
     }
+
+    void Update()
+    {
+
+        if (Input.GetKey("k"))
+        {
+           transform.Rotate (0f, 0f, +180.0f);
+        }
+
+    } */
+
 }
