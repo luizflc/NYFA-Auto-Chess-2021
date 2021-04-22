519d7f1445eb5918c15150f6d560de9a5029be35
templar45 <57818640+templar45@users.noreply.github.com>
2021-04-21 18:07:42 -0700

    Chris - icelevel update

---
+++ Assets/Script/iceSlip.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iceSlip : MonoBehaviour
+{
+    public GameObject pickupEffect;
+
+    private bool triggered;
+    public float durationOfEffect = 6f;
+    private float timer;
+    public GameObject iceBlock;
+ 
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggered)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                triggered = false;
+                timer = 0f;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerPiece") || other.CompareTag("EnemyPiece"))
+        {
+            if (!triggered)
+            {
+                Instantiate(pickupEffect, transform.position + Vector3.up, Quaternion.identity);
+                triggered = true;
+                timer = durationOfEffect;
+                Destroy(gameObject);
+            }
+
+        }
+
+    }
+}
